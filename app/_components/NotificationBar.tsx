52ab2494f7f9b113886875f439c75d0c4984017d
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-08-01 09:15:58 +0330

    Add notification bar

---
+++ app/_components/NotificationBar.tsx
@@ -0,0 +1,14 @@
+function NotificationBar() {
+  return (
+    <div className="bg-neutral-black-900 py-2">
+      <button
+        type="button"
+        className="text-neutral-white-900 w-full mx-auto text-body"
+      >
+        Get 25% OFF on your first order. Order Now
+      </button>
+    </div>
+  );
+}
+
+export default NotificationBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import NotificationBar from "./_components/NotificationBar";
+
 function Home() {
   return (
     <div>
-      <h1>e-commerce app</h1>
+      <NotificationBar />
     </div>
   );
 }
