978d1fa3c6db2aed67534ade8e655fd25a64b2e3
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-08-17 16:42:05 +0330

    Add filterByPriceRange component to enable price range filtering

---
+++ app/_components/FilterByPriceRange.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Slider } from "@/components/ui/slider";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+
+function FilterByPriceRange() {
+  const defaultValue = [0, 50];
+  const [priceRange, setPriceRange] = useState(defaultValue);
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSliderChange = useDebouncedCallback((value: number[]) => {
+    setPriceRange(value);
+
+    const params = new URLSearchParams(searchParams);
+    params.set("minPrice", String(value[0]));
+    params.set("maxPrice", String(value[1]));
+
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h4 className="text-body font-medium">Price</h4>
+      <Slider
+        defaultValue={defaultValue}
+        max={100}
+        min={0}
+        step={1}
+        onValueChange={handleSliderChange}
+      />
+      <div className="text-body">
+        Selected Range: {priceRange[0]} - {priceRange[1]}
+      </div>
+    </div>
+  );
+}
+
+export default FilterByPriceRange;
--- app/_components/FilterSidebar.tsx
+++ app/_components/FilterSidebar.tsx
@@ -1,5 +1,6 @@
 import prisma from "@/prisma/prisma";
 import FilterByCategory from "./FilterByCategory";
+import FilterByPriceRange from "./FilterByPriceRange";
 
 async function FilterSidebar() {
   const categories = await prisma.category.findMany();
@@ -7,6 +8,7 @@ async function FilterSidebar() {
   return (
     <aside className="flex flex-col gap-10 rounded-md border border-neutral-black-100 py-6 pl-4 pr-8">
       <FilterByCategory categories={categories} />
+      <FilterByPriceRange />
     </aside>
   );
 }
