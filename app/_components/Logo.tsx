2dab57f918733386fb10f749151afa87ba949ffc
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-08-31 15:40:54 +0330

    Link logo to home page

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,9 +1,12 @@
 import LogoSvg from "@/public/assets/logo.svg";
+import Link from "next/link";
 
 function Logo({ className }: { className?: string }) {
   return (
     <div className={className}>
-      <LogoSvg className="size-full" />
+      <Link href="/">
+        <LogoSvg className="size-full" />
+      </Link>
     </div>
   );
 }
