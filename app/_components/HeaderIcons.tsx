d5c0dddd50048c72f773a74fd868d18d43d07055
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-09-24 17:11:32 +0330

    Open SlidingCart component on cart icon click instead of navigating to cart page

--- app/_components/HeaderIcons.tsx
+++ app/_components/HeaderIcons.tsx
@@ -4,18 +4,28 @@ import UserIcon from "@/public/assets/User-1.svg";
 import CartIcon from "@/public/assets/Cart.svg";
 import Link from "next/link";
 import { useCart } from "../_context/cartContext/cartProvider";
+import SlidingCart from "./SlidingCart";
+import { useState } from "react";
 
 function HeaderIcons() {
   const { cart } = useCart();
+  const [isOpenCart, setIsOpenCart] = useState(false);
+
+  function handleOpenCart() {
+    setIsOpenCart(true);
+  }
 
   return (
     <div className="flex items-center justify-between gap-8">
-      <Link href={"/cart"} className="relative">
+      <div className="relative" onClick={handleOpenCart}>
         <CartIcon className="size-6 cursor-pointer" />
         <span className="absolute -right-2 -top-2 rounded-full px-1 text-xs text-neutral-black-900">
           {cart.length}
         </span>
-      </Link>
+      </div>
+
+      <SlidingCart isOpen={isOpenCart} onOpenChange={setIsOpenCart} />
+
       <Link href={"/profile"}>
         <UserIcon className="size-6 cursor-pointer" />
       </Link>
