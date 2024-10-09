3989c45a5ff5bb8813bf806248b9a70103d37527
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-10-09 10:47:41 +0330

    Add Address model and status enum in prisma

---
+++ prisma/migrations/20241008060917_/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - Added the required column `addressId` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Changed the type of `status` on the `Order` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'PROCESSING', 'SHIPPED', 'DELIVERED', 'CANCELLED');
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "addressId" INTEGER NOT NULL,
+DROP COLUMN "status",
+ADD COLUMN     "status" "OrderStatus" NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" SERIAL NOT NULL,
+    "street" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "zipCode" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
