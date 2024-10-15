03224daf15d26af0b9a73b8de7066a8f8f33bae8
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-10-15 14:36:08 +0330

    Add address page to dashboard

---
+++ app/dashboard/address/page.tsx
@@ -0,0 +1,25 @@
+import AddressForm from "@/app/checkout/AddressForm";
+import { auth } from "@/lib/auth/auth";
+import prisma from "@/prisma/prisma";
+
+async function page() {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  const address = await prisma.address.findMany({
+    where: {
+      userId: parseInt(userId),
+    },
+  });
+
+  return (
+    <div className="flex flex-col gap-6 pl-12">
+      <h2 className="text-base font-semibold">Shipping Address</h2>
+      <div className="w-[535px]">
+        <AddressForm address={address.at(0)} isCheckout={false} />
+      </div>
+    </div>
+  );
+}
+
+export default page;
