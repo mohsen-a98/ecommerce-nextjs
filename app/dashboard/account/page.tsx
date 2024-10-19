504c1b5141a9a01144005ceab8f8cdee81031482
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-10-19 16:27:33 +0330

    Display user profile picture on Google sign-in

--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -1,12 +1,14 @@
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { auth } from "@/lib/auth/auth";
 import AccountForm from "./AccountForm";
+import Image from "next/image";
 
 interface User {
   id: string;
   name: string;
   email: string;
   role: string;
+  image?: string;
 }
 
 function getInitials(name: string) {
@@ -18,12 +20,25 @@ function getInitials(name: string) {
 async function page() {
   const session = await auth();
   const user: User = session?.user;
-
   return (
     <div className="flex flex-col gap-6 pl-12">
       <h2 className="text-base font-semibold">Account Details</h2>
       <div className="mt-10 flex flex-col gap-8 sm:w-80">
         <Avatar className="size-12 rounded-full border bg-primaryPalette-100 text-primaryPalette-900">
+          <AvatarImage
+            src={user?.image}
+            referrerPolicy="no-referrer"
+            className="relative"
+            asChild
+          >
+            <Image
+              src={user?.image ?? ""}
+              fill
+              alt={user?.name}
+              className="object-cover"
+            />
+          </AvatarImage>
+
           <AvatarFallback>{getInitials(user?.name)}</AvatarFallback>
         </Avatar>
 
