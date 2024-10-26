563540cd70b500958f28b46c0a9fe2b499bf1340
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-10-26 18:55:40 +0330

    Add debounce with 300ms delay and optimize url query handling using useCallback and useMemo

--- app/_hooks/useUrlQuery.tsx
+++ app/_hooks/useUrlQuery.tsx
@@ -1,49 +1,50 @@
 import { SearchParams } from "@/lib/types";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
+import { useDebounce } from "use-debounce";
 
 function useUrlQuery(
   initialQuery: SearchParams = {},
 ): [SearchParams, (newParams: SearchParams, removeParams?: string[]) => void] {
   const [query, setQuery] = useState<SearchParams>(initialQuery);
+  const [debouncedQuery] = useDebounce(query, 300);
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  useEffect(() => {
-    setQuery(Object.fromEntries(searchParams.entries()));
+  const currentQuery = useMemo(() => {
+    return Object.fromEntries(searchParams.entries());
   }, [searchParams]);
 
-  function updateUrlParams(
-    newParams: SearchParams,
-    removeParams: string[] = [],
-  ) {
-    const mergedParams = {
-      ...query,
-      ...newParams,
-    };
+  useEffect(() => {
+    setQuery(currentQuery);
+  }, [currentQuery]);
 
-    if (removeParams.length > 0) {
-      for (const param of removeParams) {
-        delete mergedParams[param];
-      }
-    }
+  useEffect(() => {
+    const url = `${pathname}?${new URLSearchParams(debouncedQuery as any).toString()}`;
+    replace(url, { scroll: false });
+  }, [debouncedQuery, pathname, replace]);
 
-    const filteredParams = Object.fromEntries(
-      Object.entries(mergedParams)
-        .map(([key, value]) => {
-          if (Array.isArray(value)) {
-            return [key, value.join(",")];
-          }
-          return [key, value ?? ""];
-        })
-        .filter(([_, value]) => value !== ""),
-    );
+  const updateUrlParams = useCallback(
+    (newParams: SearchParams, removeParams: string[] = []) => {
+      const mergedParams = { ...query, ...newParams };
+      removeParams.forEach((param) => delete mergedParams[param]);
 
-    replace(`${pathname}?${new URLSearchParams(filteredParams).toString()}`, {
-      scroll: false,
-    });
-  }
+      const filteredParams = Object.fromEntries(
+        Object.entries(mergedParams)
+          .map(([key, value]) => {
+            if (Array.isArray(value)) {
+              return [key, value.join(",")];
+            }
+            return [key, value ?? ""];
+          })
+          .filter(([_, value]) => value !== ""),
+      ) as SearchParams;
+
+      setQuery(filteredParams);
+    },
+    [query],
+  );
 
   return [query, updateUrlParams];
 }
