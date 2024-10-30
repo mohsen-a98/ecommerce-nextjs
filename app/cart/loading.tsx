0bd2e0d7c86c0220184d6c4ff48b428adbac3b1d
mohsen-a <mohsen.amanollahi98@gmail.com>
2024-10-30 12:38:37 +0330

    Remove unnecessary <p> tag from loading component

--- app/cart/loading.tsx
+++ app/cart/loading.tsx
@@ -3,7 +3,6 @@ import Spinner from "@/app/_components/Spinner";
 function loading() {
   return (
     <div className="py-48">
-      <p className="mx-auto">for test</p>
       <Spinner />
     </div>
   );
